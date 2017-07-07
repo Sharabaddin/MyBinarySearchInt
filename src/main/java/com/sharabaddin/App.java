4b8df1100c68d3957ae9a7aad4608491e16679e1
Sharabaddin Akhmaiev <general1308@gmail.com>
2017-07-07 16:02:01 +0300

    init

--- src/main/java/com/sharabaddin/App.java
+++ src/main/java/com/sharabaddin/App.java
@@ -1,44 +1,48 @@
 package com.sharabaddin;
 
-/**
- * Enter point.
- * Jacob is a Human which open and close the door and check close or open now.
- * It`s project use polymorphism.
- * @author general1308@gmail.com
- */
+import java.util.Arrays;
+
 public class App {
+    public static int index = 0;
+
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4};
+        Arrays.binarySearch(array, 3);
 
-        //int [] myList = {"1", "2", "3"};
-        //int [] myList = (5, 8, 2);
-        //int myList [] [] = {4,9,7,0};
-        int myList [] = {4, 3, 7};
-
-
-
-        for (int i = 0; i < 2; i++) {
-            System.out.println();
-        }
-
-
-        Human jacob = new Human();
-
-        HouseDoor firstDoor = new HouseDoor();
-        RoomDoor secondDoor = new RoomDoor();
-
-        // Use the first door.
-        jacob.openDoor(firstDoor);
-        jacob.check(firstDoor);
-
-        jacob.closeDoor(firstDoor);
-        jacob.check(firstDoor);
-        jacob.closeDoor(firstDoor);
-
-        // Use the second door.
-        jacob.openDoor(secondDoor);
-        jacob.check(secondDoor);
-
-        jacob.closeDoor(secondDoor);
-        jacob.check(secondDoor);
+        System.out.println(myBinarySearch(array, 3)); //return 2
     }
+
+    public static int myBinarySearch(int[] array, int key) {
+        index = 0;
+        int[] arrayRes = difArray(array, key);
+        for (int i = array.length; i <= 1; i/=2) {
+            arrayRes = difArray(array, key);
+        }
+        return index;
+    }
+
+    private static int[] difArray(int[] array, int key) {
+        int[] resultArray;
+        if (key < array[array.length / 2]) {
+
+            // Copy array.
+            resultArray = new int[array.length / 2];
+            for (int i = 0; i < array.length / 2; i++) {
+                resultArray[i] = array[i];
+            }
+            return resultArray;
+        } else if(key >= array[array.length / 2]) {
+            index += array.length / 2;
+            // Copy array.
+            resultArray = new int[array.length / 2];
+            for (int i = array.length / 2; i > array.length; i++) {
+                resultArray[i] = array[i];
+            }
+            return resultArray;
+        } else {
+            // If found key .
+            return new int[]{key};
+        }
+    }
+
 }
