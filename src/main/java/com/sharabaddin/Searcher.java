5b1617b146a80155017a73789d4ef13a8940fa35
Sharabaddin Akhmaiev <general1308@gmail.com>
2017-07-08 14:47:03 +0300

    fix (spaces) code style

--- src/main/java/com/sharabaddin/Searcher.java
+++ src/main/java/com/sharabaddin/Searcher.java
@@ -16,12 +16,12 @@ public class Searcher {
             }
         }
 
-        // Not found
+        // Not found.
         if(key < array[0] || key > array[array.length - 1])
             return -1;
 
-        // Search middle and check <> and offset on relative (size new range);
-        int middle = (array.length+1) / 2 - 1;
+        // Search middle and check <> and offset on relative (size new range).
+        int middle = ((array.length + 1) / 2) - 1;
         for (int i = array.length; i > 1; i /= 2) {
 
             if(array[middle] == key) {
@@ -30,14 +30,14 @@ public class Searcher {
 
             if (key <= array[middle]) {
                 // Offset left.
-                middle-=(i + 2) / 4;
+                middle -= (i + 2) / 4;
             } else {
                 // Offset right.
-                middle+=(i + 2) / 4;
+                middle += (i + 2) / 4;
             }
         }
 
-        // Check found and no found in array not row
+        // Check found and no found in array not row.
         if(array[middle] == key) {
             return middle;
         } else {
